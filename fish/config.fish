7aab9f6d3989d37d698f90ba3be174b084f03836
Stefan Frede <git@frede.io>
2019-03-28 08:45:04 +0100

    Clean-up config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ and not set -q TMUX
   exec tmux
 end
 
+# Automate the installation process for fisher on a new system
 if not functions -q fisher
   set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
   curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
@@ -13,10 +14,21 @@ end
 # Set vim as editor
 set -U EDITOR vim
 
+#
+# FZF
+#
+
 # Use the new fzf keybindings
 set -U FZF_LEGACY_KEYBINDINGS 0
 
-# To get a password prompt when signing commits
+# Use ripgrep
+set -U FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+
+#
+# GPG
+#
+
+# Get a password prompt when signing commits
 set -x GPG_TTY (tty)
 
 # Start or re-use a gpg-agent.

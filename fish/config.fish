d922197492d7496495c56b3d575664357ff62f7f
Stefan Frede <stefan@frede.io>
2019-03-30 23:18:47 +0100

    Autostart SSH-agent

--- fish/config.fish
+++ fish/config.fish
@@ -25,15 +25,55 @@ set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 
 #
-# GPG
+# SSH-agent
 #
 
-# Get a password prompt when signing commits
-set -x GPG_TTY (tty)
+# Auto-launch SSH-agent
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent                                                                                                                                                                    
+  echo "Initializing new SSH agent ..."
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  echo "succeeded"
+  chmod 600 $SSH_ENV 
+  . $SSH_ENV > /dev/null
+  ssh-add
+end
+
+function test_identities                                                                                                                                                                
+  ssh-add -l | grep "The agent has no identities" > /dev/null
+  if [ $status -eq 0 ]
+    ssh-add
+    if [ $status -eq 2 ]
+      start_agent
+    end
+  end
+end
+
+if [ -n "$SSH_AGENT_PID" ] 
+  ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+  if [ $status -eq 0 ]
+    test_identities
+  end  
+else
+  if [ -f $SSH_ENV ]
+    . $SSH_ENV > /dev/null
+  end  
+  ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+  if [ $status -eq 0 ]
+    test_identities
+  else 
+    start_agent
+  end  
+end
+
+#
+# GPG
+#
 
 # Start or re-use a gpg-agent.
 gpgconf --launch gpg-agent
 
-# Ensure that GPG Agent is used as the SSH agent
-set -e SSH_AUTH_SOCK
-set -U -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+# Get a password prompt when signing commits
+set -x GPG_TTY (tty)
+

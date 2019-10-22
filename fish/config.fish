2b91f5b3ef2954b48202abafc773c74a97eb4ddc
Stefan Frede <stefan@frede.io>
2019-10-22 19:21:53 +0200

    feat: revert back to ssh from gpg

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,9 @@
-# Attach to existing or start a new TMUX session
-if status is-interactive; and not set -q TMUX
-  tmux new-session -A -s workbench
-end
-
 # Automate the installation process for fisher on a new system
 if not functions -q fisher
   set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
   curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
   fish -c fisher
 end
-
-source ~/.config/fish/aliases.fish
 
 # Set vim as editor
 set -U EDITOR vim
@@ -29,16 +22,16 @@ set -U FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 # GPG
 #
 
-# Refresh gpg-agent tty
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=835394
-if [ (pgrep -x -u $USER gpg-agent) ]
-else
-  gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
-end
-
-# Ensure that GPG Agent is used as the SSH agent
-set -e SSH_AUTH_SOCK
-set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-
 # Get a password prompt when signing commits
 set -x GPG_TTY (tty)
+
+#
+# TMUX
+#
+
+# Attach to existing or start a new TMUX session named workbench
+if status is-interactive; and not set -q TMUX
+  tmux new-session -A -s workbench
+end
+
+source ~/.config/fish/aliases.fish
--- fish/functions/unfuckgpg.fish
+++ fish/functions/unfuckgpg.fish
@@ -1,3 +1,3 @@
 function unfuckgpg
-  gpg-connect-agent updatestartuptty /bye
+  gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
 end

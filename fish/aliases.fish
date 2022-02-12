1374c82dc2290c59d63f65c608726622eba0aace
Stefan Frede <stefan@frede.dev>
2022-02-12 13:28:24 +0100

    feat: add alias for gitversion

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -4,6 +4,9 @@ alias mkdir "mkdir -p"
 # Alias the cat command to use bat instead
 alias cat "bat"
 
+# Alias the gitversion command to use docker 
+alias gitversion 'docker run --rm -v (pwd)":/repo" gittools/gitversion:5.6.10-alpine.3.12-x64-3.1 /repo'
+
 # Abbreviate commonly used functions
 # An abbreviation will expand after <space> or <Enter> is hit
 abbr g git
